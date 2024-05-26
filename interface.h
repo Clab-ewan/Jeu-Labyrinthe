0c497c5fc7611041b39f6697fbcd5bfe693eb92e
Clab-ewan <clabautewa@cy-tech.fr>
2024-05-26 02:37:01 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,4 +1,3 @@
-
 #ifndef Interface_h
 #define Interface_h
 
@@ -23,24 +22,18 @@ typedef struct {
 } Cible;
 
 void NombreJoueurs(int *nb_joueur);
-void ChoisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
+void choisirRobotCible(char **grille, int hauteur, int largeur, Robot *robot,
                        Cible *cible);
 void chronometrer(int duree_chrono);
 int choixdifficulte(int niveau_difficulte);
-void choix_joueur(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
-          Cible *cible, int *player);
+void choix_player(int **nmbMouv, int m_bJoueur, char **grille, Robot *robot,
+                  Cible *cible, int *player);
 int MIN(int My_array[], int len);
 
-void deplacement(Robot *robot, Cible *cible, int direction,
-                      MurInterdit *murInterdits, int nombreMursInterdits, char ** grille, int hauteur, int largeur);
-int choix_direction(int direction);
- void JoueurTour(char **grille, int hauteur, int largeur, Robot *robot, Cible *cible, MurInterdit *mursInterdits, int nombreMursInterdits, int *nmbMouv, int joueurActuel, int *pointsJoueurs, int **MurRandH,
- int **MurRandV, int MurH_Cibles[CIBLES][2],
- int MurV_Cibles[CIBLES][2]);
+void deplacement(Robot *robot, Cible *cible, int direction, int *MurRandH, int *MurRandV, MurCible *murHCible, MurCible *murVCible, char **grille, int hauteur, int largeur);
 
-void Points(int *pointsJoueurs, int nb_joueurs, int joueurActuel, int nb_deplacements, int nb_deplacements_effectues, int robotAtteintCible);
+int choix_direction();
 
-void AfficherGagnant(int *pointsJoueurs, int nb_joueurs);
 #endif
 
 
