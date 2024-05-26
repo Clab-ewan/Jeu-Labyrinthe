57db094a3bc1413cdd2d85618fa8bcf1dcaabcd3
Clab-ewan <clabautewa@cy-tech.fr>
2024-05-26 21:00:18 +0200

    Update creerGrille.c

--- creerGrille.c
+++ creerGrille.c
@@ -10,8 +10,7 @@ void clearInputBuffer() {
 }
 
 // Fonction pour initialiser la grille
-void initialiserGrille(char ***grille, int hauteur,
-                       int largeur) { // Checker hauteur et largeur
+void initialiserGrille(char ***grille, int hauteur, int largeur) {
   *grille = (char **)malloc(hauteur * sizeof(char *));
   for (int i = 0; i < hauteur; i++) {
     (*grille)[i] = (char *)malloc(largeur * sizeof(char));
@@ -43,11 +42,12 @@ int estPositionValide(char **grille, int hauteur, int largeur, int ligne,
 void placerCibles(char **grille, int hauteur, int largeur, int CordCibles[][2],
                   MurCible murHCible[CIBLES], MurCible murVCible[CIBLES]) {
   for (int num = 0; num < CIBLES; num++) {
-    int ligne, col;
-    while (!estPositionValide(grille, hauteur, largeur, ligne, col)) {
+    int ligne = 0, col = 0;
+    do {
       ligne = rand() % (hauteur - 2) + 1;
       col = rand() % (largeur - 2) + 1;
-    };
+    } while (!estPositionValide(grille, hauteur, largeur, ligne, col));
+
     grille[ligne][col] = 'A' + num;
     CordCibles[num][0] = ligne;
     CordCibles[num][1] = col;
@@ -59,24 +59,32 @@ void placerCibles(char **grille, int hauteur, int largeur, int CordCibles[][2],
       murHCible[num].col = col;
       murVCible[num].ligne = ligne;
       murVCible[num].col = col;
+      murHCible[num].cas = 0;
+      murVCible[num].cas = 0;
       break;
     case 1: // Haut Droite
       murHCible[num].ligne = ligne;
       murHCible[num].col = col;
       murVCible[num].ligne = ligne;
       murVCible[num].col = col + 1;
+      murHCible[num].cas = 0;
+      murVCible[num].cas = 1;
       break;
     case 2: // Bas Gauche
       murHCible[num].ligne = ligne + 1;
       murHCible[num].col = col;
       murVCible[num].ligne = ligne;
       murVCible[num].col = col;
+      murHCible[num].cas = 1;
+      murVCible[num].cas = 0;
       break;
     case 3: // Bas Droite
       murHCible[num].ligne = ligne + 1;
       murHCible[num].col = col;
       murVCible[num].ligne = ligne;
       murVCible[num].col = col + 1;
+      murHCible[num].cas = 1;
+      murVCible[num].cas = 1;
       break;
     default:
       printf("Erreur d'allocation MurCibles\n");
@@ -99,16 +107,15 @@ void placerRobots(char **grille, int hauteur, int largeur) {
 }
 
 // Fonction pour afficher la grille
-void afficherGrille(char **grille, int hauteur, int largeur, int **MurRandH,
-                    int **MurRandV, MurCible murHCible[CIBLES],
+void afficherGrille(char **grille, int hauteur, int largeur, int *MurRandH,
+                    int *MurRandV, MurCible murHCible[CIBLES],
                     MurCible murVCible[CIBLES]) {
-  couleur("47"); // Fond blanc
-  for (int ligne = 0; ligne <= hauteur; ligne++) {
+  couleur("47");                                  // Fond blanc
+  for (int ligne = 0; ligne < hauteur; ligne++) { // Correction ici
     // Afficher la bordure supérieure de chaque cellule
     for (int col = 0; col < largeur; col++) {
-      if ((ligne == 0) || (ligne == hauteur)) {
-        couleur("31"); // Texte rouge pour les lignes des bords supérieur et
-                       // inférieur
+      if (ligne == 0) {
+        couleur("31"); // Texte rouge pour la ligne supérieure
       } else {
         couleur("34"); // Texte bleu pour les lignes internes
       }
@@ -116,17 +123,14 @@ void afficherGrille(char **grille, int hauteur, int largeur, int **MurRandH,
         couleur("31"); // Texte rouge pour la bordure gauche et droite
       }
       printf("+");
-      if ((ligne == 0) || ligne == hauteur) {
-        couleur("31"); // Texte rouge pour les lignes des bords supérieur et
-                       // inférieur
-      } else if (ligne == (*MurRandH)[0] && col == 0 ||
-                 ligne == (*MurRandH)[1] && col == 0 ||
-                 ligne == (*MurRandH)[2] && col == largeur - 1 ||
-                 ligne == (*MurRandH)[3] && col == largeur - 1) {
-        couleur("31"); // Texte rouge pour les 4 mur horizontaux Rand
-      }
-
-      else {
+      if (ligne == 0) {
+        couleur("31"); // Texte rouge pour la ligne supérieure
+      } else if (ligne == MurRandH[0] && col == 0 ||
+                 ligne == MurRandH[1] && col == 0 ||
+                 ligne == MurRandH[2] && col == largeur - 1 ||
+                 ligne == MurRandH[3] && col == largeur - 1) {
+        couleur("31"); // Texte rouge pour les 4 murs horizontaux Rand
+      } else {
         couleur("34"); // Texte bleu pour les lignes internes
       }
       for (int i = 0; i < CIBLES; i++) {
@@ -136,20 +140,17 @@ void afficherGrille(char **grille, int hauteur, int largeur, int **MurRandH,
       }
       printf("---");
     }
-    couleur(
-        "31"); // Texte rouge pour les lignes des bords supérieur et inférieur
+    couleur("31"); // Texte rouge pour la ligne supérieure
     printf("+\n");
 
-    if (ligne == hauteur)
-      break; // Pour éviter de dessiner une ligne de contenu en trop
     // Afficher le contenu des cellules
     for (int col = 0; col < largeur; col++) {
       if (col == 0) {
         couleur("31"); // Texte rouge pour la bordure gauche et droite
-      } else if (ligne == 0 && col == (*MurRandV)[0] ||
-                 ligne == 0 && col == (*MurRandV)[1] ||
-                 ligne == hauteur - 1 && col == (*MurRandV)[2] ||
-                 ligne == hauteur - 1 && col == (*MurRandV)[3]) {
+      } else if (ligne == 0 && col == MurRandV[0] ||
+                 ligne == 0 && col == MurRandV[1] ||
+                 ligne == hauteur - 1 && col == MurRandV[2] ||
+                 ligne == hauteur - 1 && col == MurRandV[3]) {
         couleur("31"); // Texte rouge pour les 4 murs verticaux Rand
       } else {
         couleur("30"); // Texte noir pour le contenu des cellules
@@ -170,9 +171,13 @@ void afficherGrille(char **grille, int hauteur, int largeur, int **MurRandH,
     couleur("31"); // Texte rouge pour la bordure gauche et droite
     printf("|\n");
   }
+  // Afficher la bordure inférieure de la grille
+  for (int col = 0; col < largeur; col++) {
+    couleur("31"); // Texte rouge pour la ligne inférieure
+    printf("+---");
+  }
+  printf("+\n");
   couleur("0");
-}
-  (*nombreMursInterdits) += 4;
 }
 
 
